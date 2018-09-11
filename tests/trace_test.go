7fdf256a8caec711b5561fe7eadd37fb5340c381
Sunny Klair <sunny.klair@datadoghq.com>
2018-09-11 14:21:40 -0400

    Add straightforward TCP send/recv test

--- tests/trace_test.go
+++ tests/trace_test.go
@@ -13,9 +13,67 @@ import (
 	"time"
 
 	"github.com/DataDog/tcptracer-bpf/pkg/tracer"
+	"github.com/stretchr/testify/assert"
 )
 
 var payloadSizes = []int{2 << 5, 2 << 8, 2 << 10, 2 << 12, 2 << 14, 2 << 15}
+
+func TestTCPSendAndReceiveWithBPF(t *testing.T) {
+	ClientMessageSize := 2 << 8
+	ServerMessageSize := 2 << 15
+
+	// Enable BPF-based network tracer
+	tr, err := tracer.NewTracer(tracer.DefaultConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tr.Start()
+	defer tr.Stop()
+
+	// Create TCP Server which sends back ServerMessageSize bytes
+	server := NewServer(func(c net.Conn) {
+		r := bufio.NewReader(c)
+		r.ReadBytes(byte('\n'))
+		c.Write(genPayload(ServerMessageSize))
+		c.Close()
+	})
+	doneChan := make(chan struct{})
+	server.Run(doneChan)
+
+	c, err := net.DialTimeout("tcp", server.address, 50*time.Millisecond)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	// Write ClientMessageSize to server, and read response
+	if _, err = c.Write(genPayload(ClientMessageSize)); err != nil {
+		t.Fatal(err)
+	}
+	r := bufio.NewReader(c)
+	r.ReadBytes(byte('\n'))
+
+	// Iterate through active connections until we find connection created above, and confirm send + recv counts
+	connectionFound := false
+	connections, err := tr.GetActiveConnections()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, conn := range connections.Conns {
+		localAddr := fmt.Sprintf("%s:%d", conn.Source, conn.SPort)
+		remoteAddr := fmt.Sprintf("%s:%d", conn.Dest, conn.DPort)
+		if localAddr == c.LocalAddr().String() && remoteAddr == c.RemoteAddr().String() {
+			connectionFound = true
+			assert.Equal(t, ClientMessageSize, int(conn.SendBytes))
+			assert.Equal(t, ServerMessageSize, int(conn.RecvBytes))
+		}
+	}
+
+	assert.True(t, connectionFound)
+
+	doneChan <- struct{}{}
+}
 
 func runBenchtests(b *testing.B, prefix string, f func(p int) func(*testing.B)) {
 	for _, p := range payloadSizes {
