b851ebd7572d38a28f46e9944d2cacf30850e2f2
zandrewitte <zwitte@stackstate.com>
2019-01-09 14:41:07 +0100

    integration windows tracing into tcp tracer

--- pkg/tracer/common/common.go
+++ pkg/tracer/common/common.go
@@ -1,22 +1,7 @@
 package common
 
-import (
-	"errors"
-	bpflib "github.com/iovisor/gobpf/elf"
-)
+import "errors"
 
 var (
 	ErrNotImplemented = errors.New("BPF-based network tracing not implemented on non-linux systems")
 )
-
-// KERNEL_VERSION(a,b,c) = (a << 16) + (b << 8) + (c)
-// Per https://github.com/torvalds/linux/blob/master/Makefile#L1187
-func LinuxKernelVersionCode(major, minor, patch uint32) uint32 {
-	return (major << 16) + (minor << 8) + patch
-}
-
-// CurrentKernelVersion exposes calculated kernel version - exposed in LINUX_VERSION_CODE format
-// That is, for kernel "a.b.c", the version number will be (a<<16 + b<<8 + c)
-func CurrentKernelVersion() (uint32, error) {
-	return bpflib.CurrentKernelVersion()
-}
