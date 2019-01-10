e8152d556858f30e891b401c7a5535e074884f6a
Zandre Witte <zandre.witte@gmail.com>
2019-01-10 17:08:40 +0100

    added gobpf updated

--- vendor/github.com/iovisor/gobpf/elf/elf.go
+++ vendor/github.com/iovisor/gobpf/elf/elf.go
@@ -50,7 +50,6 @@ import (
 #include <sys/socket.h>
 #include <linux/unistd.h>
 #include "include/bpf.h"
-#include "include/bpf_map.h"
 #include <poll.h>
 #include <linux/perf_event.h>
 #include <sys/resource.h>
