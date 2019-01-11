aff366197fa2c28b2fae0e52532f51e632f971d1
zandrewitte <zwitte@stackstate.com>
2019-01-11 13:13:24 +0100

    agent windows test application

---
+++ cmd/agent_windows.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/StackVista/tcptracer-bpf/pkg/tracer"
+	"github.com/StackVista/tcptracer-bpf/pkg/tracer/config"
+)
+
+func main () {
+
+	tracerConfig := config.MakeDefaultConfig()
+	t, err := tracer.NewTracer(tracerConfig)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	conns, err := t.GetConnections()
+
+	for _, c := range conns.Conns {
+		fmt.Println(c.String())
+	}
+}
