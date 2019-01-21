2c143846ec29e8ac46ffedc60df3bdbbdd144211
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2019-01-21 12:06:09 +0100

    feature(windows)[STAC-3400] Install headers in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,8 @@ sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common \
+    linux-headers-$(uname -r)
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
